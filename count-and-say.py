6d486b6eb214738ac11149c1b4f2f558c75aa43c
Zhaoyang <lizy14@users.noreply.github.com>
2017-01-18 06:43:25 +0800

    Update count-and-say.py

--- count-and-say.py
+++ count-and-say.py
@@ -7,30 +7,23 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-
-        s = '1'
-        for i in range(n - 1):
-            s = self.read(s)
-        return s
-
-    def read(self, s):
-        """
-        :type n: str
-        :rtype: str
-        """
         
         # 'AAAABBBCCD' -> [[4, 'A'], [3, 'B'], [2, 'C'], [1, 'D']]
-        group = lambda x: [[len(list(g)), k] for k, g in groupby(s)]
+        group = lambda _str: [[len(list(g)), k] for k, g in groupby(_str)]
 
         # [4, 'A'] -> '4A'
-        make_phrase = lambda x: ''.join(map(str, x))
-        
-        return ''.join(make_phrase(g) for g in group(s))
+        make_phrase = lambda _arr: ''.join(map(str, _arr))
 
+        # 'AAAABBBCCD' -> '4A3B2C1D'
+        make_sentence = lambda _str: ''.join(make_phrase(g) for g in group(_str))
+        
+        s = '1'
+        for i in range(n - 1):
+            s = make_sentence(s)
+        return s
 
         
 if __name__ == "__main__":
     solution = Solution()
-    assert(solution.read('1211') == '111221')
     for i in range(10):
         print(solution.countAndSay(i))
