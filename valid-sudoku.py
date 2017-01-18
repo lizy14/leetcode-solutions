df795f56df4bf16cc4092d4ed8c54510b95d0ae5
Zhaoyang <lizy14@users.noreply.github.com>
2017-01-18 08:54:42 +0800

    Update valid-sudoku.py

--- valid-sudoku.py
+++ valid-sudoku.py
@@ -1,43 +1,41 @@
+class InvalidSoduku(BaseException):
+    pass
+
 class Solution(object):
+
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: bool
         """
 
-        def _and(arr):
-            for a in arr:
-                if not a:
-                    return False
-            return True
-
         rg = range(9)
+        srg = range(3)
 
         def one_to_nine(arr):
-            def count(arr, i):
-                return len(list(filter(lambda x: x == i, arr)))
-            return _and(count(arr, str(i + 1)) <= 1 for i in rg)
+            cells = [i for i in arr if i != '.']
+            if len(cells) != len(set(cells)):
+                raise InvalidSoduku()
         
-        def validate_row(i):
-            return one_to_nine([board[i][j] for j in rg])
+        def validate_row():
+            [one_to_nine([board[i][j] for j in rg]) for i in rg]
 
-        def validate_column(j):
-            return one_to_nine([board[i][j] for i in rg])
-            
-        def get_coord(id):
-            return (id % 3, id // 3)
+        def validate_column():
+            [one_to_nine([board[i][j] for i in rg]) for j in rg]
 
-        def validate_subbox(box_id):
-            def get_cell(box_id, sub_id):
-                (sub_x, sub_y) = get_coord(sub_id)
-                (box_x, box_y) = get_coord(box_id)
-                return board[box_x * 3 + sub_x][box_y * 3 + sub_y]
-            cells = [get_cell(box_id, sub_id) for sub_id in rg]
-            return one_to_nine(cells)
+        def validate_subbox():
+            for box_x in srg:
+                for box_y in srg:
+                    one_to_nine([
+                        board[box_x * 3 + sub_x][box_y * 3 + sub_y]
+                        for sub_x in range(3)
+                        for sub_y in range(3)
+                    ])
 
-        columns = _and([validate_column(i) for i in rg])
-        rows = _and([validate_row(i) for i in rg])
-        subboxes = _and([validate_subbox(i) for i in rg])
-
-        return _and([columns, rows, subboxes])
-        
+        try:
+            validate_column()
+            validate_row()
+            validate_subbox()
+            return True
+        except InvalidSoduku:
+            return False
