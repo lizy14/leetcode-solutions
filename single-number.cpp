eee2d2975a5764cac3f116d5e3bfad65e8860412
Zhaoyang <lizy14@users.noreply.github.com>
2017-03-21 17:25:00 +0800

    Create single-number.cpp

---
+++ single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x = 0;
+        for(auto num : nums){
+            x ^= num;
+        }
+        return x;
+    }
+};
