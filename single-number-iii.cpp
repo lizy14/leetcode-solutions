7abad610d6b0669cb9a6e4242ab0ce65357aa971
Zhaoyang <lizy14@users.noreply.github.com>
2017-03-21 17:43:23 +0800

    Update single-number-iii.cpp

--- single-number-iii.cpp
+++ single-number-iii.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
+        int x = 0;
+        for(auto num: nums){
+            x ^= num;
+        }
+        int mask = x & -x; // least significant 1
+        int a = 0, b = 0;
+        for(auto num: nums){
+            if(num & mask){
+                a ^= num;
+            }else{
+                b ^= num;
+            }
+        }
+        
+        vector<int> result = {a, b};
+        return result;
+    }
+    vector<int> singleNumberBruteForce(vector<int>& nums) {
         vector<int> result;
         map<int, int> m;
         for(auto num: nums){
