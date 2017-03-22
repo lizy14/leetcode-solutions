f85bc0e53171023b216dd6f16811ed39eddf839f
Zhaoyang <lizy14@users.noreply.github.com>
2017-03-22 21:46:11 +0800

    Update move-zeroes.cpp

--- move-zeroes.cpp
+++ move-zeroes.cpp
@@ -3,15 +3,14 @@ public:
     void moveZeroes(vector<int>& nums) {
         auto n = nums.size();
         int counter = 0;
-        for(int i=0; i<n-counter; i++){
-            if(nums[i] == 0){
+        for(int i=0; i<n; i++){
+            nums[counter] = nums[i];
+            if(nums[i] != 0){
                 counter++;
-                for(int j=i; j<=n-counter; j++){
-                    nums[j] = nums[j+1];
-                }
-                nums[n-1] = 0;
-                i--;
             }
+        }
+        for(int i=counter; i<n; i++){
+            nums[i] = 0;
         }
     }
 };
