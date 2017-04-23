1baf7c5513272858105438a67110f5b5fb2130e7
Zhaoyang <lizy14@users.noreply.github.com>
2017-04-23 11:06:35 +0800

    Create permutations-ii.cpp

---
+++ permutations-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<int>> result;
+        
+        vector<int> original = nums;
+        
+        do{
+            result.push_back(nums);
+            nextPermutation(nums);
+        }while(original != nums);
+        
+        return result;
+        
+    }
+    
+private:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        
+        int m = n - 1;
+        // invariant: [m, n) in decreasing order
+        while(m > 0){
+            if(nums[m - 1] >= nums[m]){
+                m --;
+            }else{
+                break;
+            }
+        }
+        
+        if(m == 0){
+            std::reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        int c = nums[m - 1];
+        
+        int s = nums[m]; // the smallest and last in [m, n) to > c
+        int k = m; // subscript of s
+        
+        for(int i = m + 1; i < n; i ++){
+            auto a = nums[i];
+            if(a > c && a <= s){
+                s = a;
+                k = i;
+            }
+        }
+        
+        std::swap(nums[k], nums[m - 1]);
+        std::reverse(nums.begin() + m, nums.end());
+        return;
+    }
+};
