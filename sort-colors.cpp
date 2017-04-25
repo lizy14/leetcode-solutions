3e5e0bd19d8d98bb720ed6a1370fa0224e029321
Zhaoyang <lizy14@users.noreply.github.com>
2017-04-25 19:40:18 +0800

    Update sort-colors.cpp

--- sort-colors.cpp
+++ sort-colors.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
+        int counters[3] = {0, 0, 0};
+        for(auto n : nums){
+            counters[n] ++;
+        }
+        int subscript = 0;
+        for(auto j=0; j<3; j++){
+            for(auto i=0; i<counters[j]; i++){
+                nums[subscript++] = j;
+            }
+        }
     }
 };
