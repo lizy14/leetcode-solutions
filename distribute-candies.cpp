c7ba359d59432ba5a227fd15f271c09b60772c5d
Zhaoyang <lizy14@users.noreply.github.com>
2017-05-21 20:56:49 +0800

    Update distribute-candies.cpp

--- distribute-candies.cpp
+++ distribute-candies.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candies) {
         vector<int> u = candies;
+        sort(u.begin(), u.end());
         u.erase(unique(u.begin(), u.end()), u.end()); 
         return min(u.size(), candies.size() / 2);
     }
